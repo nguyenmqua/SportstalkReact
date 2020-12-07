6b87a2314792c43eaddbb9f3f8b4f3cee468aa8a
Qua Nguyen <Nguyenmqua@gmail.com>
2020-12-07 08:11:35 -0700

    uploading to heroku

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -3,9 +3,10 @@ const db = require("../models")
 module.exports = {
     updateByID: function(req,res){
         console.log(req.body)
-        db.Bets
-        .findOneAndUpdate({_id: req.body.id}, {completed: true}, { new: true },)
-        .then(DBpost => res.json(DBpost))
+        db.Notifications
+        .remove({sportTicket: req.body.id, userId: req.body.userId})       
+        db.Bets.findOneAndUpdate({_id: req.body.id}, {completed: true}, { new: true },)
+        .then(dbdata=> res.json(dbdata))
         .catch(err => res.status(422).json(err));
     }
 
--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const notificationsController = require("../../controllers/notificationsController")
 
+router.route("/")
+    .post(notificationsController.create)
+
 router.route("/:id")
     .get(notificationsController.get)
     .delete(notificationsController.remove)
